3eb672f97467a66dfde3f1eee258d58b9ea15d1d
Miguel Palhas <mpalhas@gmail.com>
2017-02-24 18:07:43 +0000

    Prevent line breaks in the wrong places

--- data/speakers.js
+++ data/speakers.js
@@ -7,5 +7,5 @@ export default [
   { name: 'Brad Frost', bio: 'Author of Atomic Design', image: brad },
   // { name: 'Claudio Guglieri', bio: 'Design Director at Microsoft', image: claudio },
   // { name: 'Una Kravets', bio: 'UI Engineer at DigitalOcean', image: una },
-  { name: 'C Todd Lombardo', bio: 'Chief Design Strategist at Fresh Tilled Soil', image: todd },
+  { name: 'C Todd Lombardo', bio: 'Chief Design Strategist at Fresh\u00a0Tilled\u00a0Soil', image: todd },
 ];
