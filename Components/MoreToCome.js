1d103367ea87793e58aeec85be5cabbcb966f0aa
João Justo <jpjustonunes@gmail.com>
2017-02-24 17:27:14 +0000

    Add more to come to speakers list

---
+++ Components/MoreToCome.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import '../css/Components/MoreToCome';
+
+import Text from './Text';
+
+const MoreToCome = () => (
+  <div className="MoreToCome">
+    <div className="MoreToCome-details">
+      <h3 className="MoreToCome-name">More to come</h3>
+      <Text>Stay Tunned</Text>
+    </div>
+  </div>
+);
+
+export default MoreToCome;
