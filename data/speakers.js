a44e99f37f2c9a5e932aaf823d3efc67c188187c
Miguel Palhas <mpalhas@gmail.com>
2017-03-24 10:59:05 +0000

    Adds two new speakers

--- data/speakers.js
+++ data/speakers.js
@@ -1,11 +1,11 @@
-// import una from '../images/speakers/una.jpg';
 import todd from '../images/speakers/todd.jpg';
 import brad from '../images/speakers/brad.jpg';
-// import claudio from '../images/speakers/claudio.jpg';
+import claudio from '../images/speakers/claudio.jpg';
+import jeremy from '../images/speakers/jeremy.jpg';
 
 export default [
   { name: 'Brad Frost', bio: 'Author of Atomic Design', image: brad },
-  // { name: 'Claudio Guglieri', bio: 'Design Director at Microsoft', image: claudio },
-  // { name: 'Una Kravets', bio: 'UI Engineer at DigitalOcean', image: una },
-  { name: 'C Todd Lombardo', bio: 'Chief Design Strategist at Fresh\u00a0Tilled\u00a0Soil', image: todd },
+  { name: 'C Todd Lombardo', bio: 'Chief Design Strategist at\u00aFresh\u00a0Tilled\u00a0Soil', image: todd },
+  { name: 'Claudio Guglieri', bio: 'Design Director at Microsoft', image: claudio },
+  { name: 'Jeremy Keith', bio: 'Co-founder and technical lead\u00aat\u00aClearleft', image: jeremy },
 ];
