1022db9e9d7b6f3ec082ccf087db44016ebbfe62
João Justo <jpjustonunes@gmail.com>
2017-04-04 16:40:25 +0100

    HOTFIX: copy typo

--- Components/MoreToCome.js
+++ Components/MoreToCome.js
@@ -8,7 +8,7 @@ const MoreToCome = () => (
   <div className="MoreToCome">
     <div className="MoreToCome-details">
       <h3 className="MoreToCome-name">More to come</h3>
-      <Text>Stay Tunned</Text>
+      <Text>Stay Tuned</Text>
     </div>
   </div>
 );
