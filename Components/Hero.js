d504dffc047ea1d58034f742e6c896d375c8ca28
Miguel Palhas <mpalhas@gmail.com>
2017-04-11 12:33:56 +0100

    Moves video background to a Youtube embedded version
    Due to bandwidth pricing, we're moving the video file (which is by far
    the most consuming resource on Clouflare) to Youtube
    
    An existing package was used to embed the video, with callbacks for when
    the playback is started (after loading), so that we can show
    a placeholder image beforehand

--- Components/Hero.js
+++ Components/Hero.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import classNames from 'classnames';
+import Youtube from 'react-youtube';
 
 import '../css/Components/Hero';
 import '../css/Components/Grid';
@@ -8,81 +10,104 @@ import TextTitle from './TextTitle';
 import Mirrored from './Mirrored';
 import SocialLinks from './SocialLinks';
 
-import HeroVideo from '../pages/hero.mp4';
-import HeroPoster from '../pages/hero-cover.jpg';
 import HeroFallback from '../pages/hero.jpg';
 import PlayIcon from '../images/play.svg';
 
-const Hero = () => (
-  <section className="Hero">
-    <div className="Hero-background">
-      <Mirrored id="video" hide>
-        <video
-          loop
-          muted
-          autoPlay
-          playsInline
-          preload="auto"
-          type="video/mp4"
-          poster={HeroPoster}
-          className="Hero-video"
-          ref={video => video.play}
-        >
-          <source src={HeroVideo} />
-          <source src={HeroFallback} />
-        </video>
-      </Mirrored>
-      <Mirrored id="video" hide>
-        <div className="Hero-backgroundOverlay" />
-      </Mirrored>
-    </div>
-
-    <Section>
-      <div className="Hero-foreground">
-        <div className="Hero-content Grid Grid--1offset">
-          <div className="Hero-title">
-            <h1 className="Hero-headline">Mirror Conf 2017</h1>
-            <h2 className="Hero-uvp">
-              For designers and front-end developers.
-            </h2>
-            <h3 className="Hero-date">
-              October 10 — 13, 2017<br />
-              Braga, Portugal
-            </h3>
-
-            <h4 className="Hero-fullDate">
-              Oct. 10 & 11 — workshops,<br />
-              12 & 13 — main conference
-            </h4>
-          </div>
-
-          <div className="Hero-actions">
-            <Mirrored id="button" hover>
-              <a href="https://www.youtube.com/watch?v=JWa0PMXN7ZE" className="Hero-playButton" target="blank">
-                <img src={PlayIcon} alt="Play icon" className="Hero-playButtonImage" />
-                <div className="Hero-playButtonLabel">
-                  <TextTitle>
-                    1st edition<br />
-                    best of
-                  </TextTitle>
-                </div>
-              </a>
-            </Mirrored>
-          </div>
-        </div>
-
-        <div className="Hero-footer">
-          <SocialLinks />
-          <div className="Hero-footerSpace" />
-          <TextTitle alternate>Follow us</TextTitle>
-        </div>
-      </div>
-    </Section>
-  </section>
-);
-
-Hero.componentDidMount = () => {
-
+const YoutubeVideoId = "B7bqAsxee4I"
+const youtubeOpts = {
+  playerVars: {
+    autoplay: 1,
+    loop: 1,
+    controls: 0,
+    playlist: YoutubeVideoId,
+  },
 };
 
-export default Hero;
+
+export default class Hero extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { playing: false };
+  }
+
+  componentDidMount() {
+  }
+
+  onYoutubePlay = () => {
+    this.setState({ playing: true });
+  }
+
+  classes() {
+    return classNames({
+      Hero: true,
+      'is-video-playing': this.state.playing,
+    });
+  }
+
+  render() {
+    return (
+      <section className={this.classes()}>
+        <div className="Hero-background">
+          <Mirrored id="video-fallback" hide>
+            <img className="Hero-videoFallback" src={HeroFallback} alt="MirrorConf 2016 video placeholder" />
+          </Mirrored>
+          <Mirrored id="video" disable>
+            <div className="Hero-videoWrapper">
+              <Youtube
+                className="Hero-video"
+                videoId={YoutubeVideoId}
+                onPlay={this.onYoutubePlay}
+                opts={youtubeOpts}
+              />
+            </div>
+          </Mirrored>
+          <Mirrored id="overlay" hide>
+            <div className="Hero-backgroundOverlay" />
+          </Mirrored>
+        </div>
+
+        <Section>
+          <div className="Hero-foreground">
+            <div className="Hero-content Grid Grid--1offset">
+              <div className="Hero-title">
+                <h1 className="Hero-headline">Mirror Conf 2017</h1>
+                <h2 className="Hero-uvp">
+                  For designers and front-end developers.
+                </h2>
+                <h3 className="Hero-date">
+                  October 10 — 13, 2017<br />
+                  Braga, Portugal
+                </h3>
+
+                <h4 className="Hero-fullDate">
+                  Oct. 10 & 11 — workshops,<br />
+                  12 & 13 — main conference
+                </h4>
+              </div>
+
+              <div className="Hero-actions">
+                <Mirrored id="button" hover>
+                  <a href="https://www.youtube.com/watch?v=JWa0PMXN7ZE" className="Hero-playButton" target="blank">
+                    <img src={PlayIcon} alt="Play icon" className="Hero-playButtonImage" />
+                    <div className="Hero-playButtonLabel">
+                      <TextTitle>
+                        1st edition<br />
+                        best of
+                      </TextTitle>
+                    </div>
+                  </a>
+                </Mirrored>
+              </div>
+            </div>
+
+            <div className="Hero-footer">
+              <SocialLinks />
+              <div className="Hero-footerSpace" />
+              <TextTitle alternate>Follow us</TextTitle>
+            </div>
+          </div>
+        </Section>
+      </section>
+    );
+  }
+}
--- Components/Mirrored.js
+++ Components/Mirrored.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
-const Mirrored = ({ children, id, hover, hide }) => {
+const Mirrored = ({ children, id, hover, hide, disable }) => {
   const finalProps = {
     'data-mirror-id': id,
     'data-mirror-hide': hide,
     'data-mirror-hover': hover,
+    'data-mirror-disable': disable,
   };
 
   return React.cloneElement(children, finalProps);
