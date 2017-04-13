a73d23f7db93dc82907d983a2a3bbb22ab2b6900
Fernando Mendes <devfrmendes@gmail.com>
2017-04-13 11:28:37 +0100

    Adds Usersnap as media sponsor

--- data/sponsors.js
+++ data/sponsors.js
@@ -2,6 +2,7 @@ import KirbyLogo from '../images/sponsors/kirby.svg';
 import SeegnoLogo from '../images/sponsors/seegno.svg';
 import AdorableLogo from '../images/sponsors/adorable.svg';
 import PixelMattersLogo from '../images/sponsors/pixelmatters.svg';
+import UsersnapLogo from '../images/sponsors/usersnap.svg';
 
 export default [
   {
@@ -16,6 +17,12 @@ export default [
       { id: 2, alt: 'Kirby logo', src: KirbyLogo, href: 'https://getkirby.com/' },
       { id: 3, alt: 'Adorable logo', src: AdorableLogo, href: 'http://adorable.io/' },
       { id: 4, alt: 'Pixelmatters logo', src: PixelMattersLogo, href: 'http://pixelmatters.com/' },
+    ],
+  },
+  {
+    title: 'Media',
+    logos: [
+      { id: 5, alt: 'Usersnap logo', src: UsersnapLogo, href: 'https://usersnap.com/' },
     ],
   },
 ];
