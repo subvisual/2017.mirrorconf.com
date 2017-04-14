0ab84397714037903b6b95b9213641702a9eb206
Miguel Palhas <mpalhas@gmail.com>
2017-04-14 11:08:32 +0100

    Removes mirroring of hover effect
    This is only being used in a single button, which is not even in
    a noticeable position, so we're just hurting performance with
    unnecessary mouse events

--- Components/Mirror.js
+++ Components/Mirror.js
@@ -1,44 +1,23 @@
-/* global document */
-
-import React, { Component, PropTypes } from 'react';
-import _ from 'lodash';
+import React, { PropTypes } from 'react';
 
 import '../css/Components/Mirror';
 
-export default class Mirror extends Component {
-  static propTypes = {
-    children: PropTypes.node.isRequired,
-  }
-
-  componentDidMount = () => {
-    _.each(document.querySelectorAll('.Mirror-original [data-mirror-hover]'), this.mirrorEventsOn);
-  }
-
-  findReflectedElement = (el) => {
-    const id = el.dataset.mirrorId;
-    return document.querySelector(`.Mirror-reflection [data-mirror-id=${id}]`);
-  }
-
-  mirrorEventsOn = (el) => {
-    const reflection = this.findReflectedElement(el);
-
-    el.addEventListener('mouseenter', () => reflection.classList.add('hover'));
-    el.addEventListener('mouseleave', () => reflection.classList.remove('hover'));
-  }
-
-  render() {
-    return (
-      <div className="Mirror">
-        <div className="Mirror-original">
-          {this.props.children}
-        </div>
-        <div className="Mirror-container">
-          <div className="Mirror-cut" />
-          <div className="Mirror-reflection">
-            {this.props.children}
-          </div>
-        </div>
+const Mirror = ({ children }) => (
+  <div className="Mirror">
+    <div className="Mirror-original">
+      {children}
+    </div>
+    <div className="Mirror-container">
+      <div className="Mirror-cut" />
+      <div className="Mirror-reflection">
+        {children}
       </div>
-    );
-  }
-}
+    </div>
+  </div>
+);
+
+Mirror.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Mirror;
