c1fc0a1039a4db55824c2ce74300e156987e9ca8
Miguel Palhas <mpalhas@gmail.com>
2017-05-22 14:11:11 +0100

    Hides ProgramCTA for now

--- Components/ProgramCta.js
+++ Components/ProgramCta.js
@@ -10,22 +10,23 @@ import WhiteBox from '../Components/WhiteBox';
 import SubSectionTitle from '../Components/SubSectionTitle';
 
 const ProgramCta = () => (
-  <Section>
-    <WhiteBox id="ProgramCta">
-      <div className="ProgramCta-title">
-        <SubSectionTitle alternate>Explore the program</SubSectionTitle>
-      </div>
-      <div className="ProgramCta-description">
-        <Text alternative>
-          We have prepared a program with activities that will get you to
-          know better the beautiful north of Portugal. Between Braga
-          and Porto you will have time to explore in the days before
-          and axfter the conference.
-        </Text>
-      </div>
-      <Button alternative>EXPLORE PROGRAM</Button>
-    </WhiteBox>
-  </Section>
+  null
+  // <Section>
+  //   <WhiteBox id="ProgramCta">
+  //     <div className="ProgramCta-title">
+  //       <SubSectionTitle alternate>Explore the program</SubSectionTitle>
+  //     </div>
+  //     <div className="ProgramCta-description">
+  //       <Text alternative>
+  //         We have prepared a program with activities that will get you to
+  //         know better the beautiful north of Portugal. Between Braga
+  //         and Porto you will have time to explore in the days before
+  //         and axfter the conference.
+  //       </Text>
+  //     </div>
+  //     <Button alternative>EXPLORE PROGRAM</Button>
+  //   </WhiteBox>
+  // </Section>
 );
 
 export default ProgramCta;
