b3be46a98dd574a9a388aa3816a51bcfe2deda2a
Miguel Palhas <mpalhas@gmail.com>
2017-05-26 11:54:48 +0100

    New workshops

--- data/workshops.js
+++ data/workshops.js
@@ -1,7 +1,11 @@
 import atomicImage from '../images/workshops/atomic_workshop.jpg';
 import productImage from '../images/workshops/product_workshop.jpg';
+import saraImage from '../images/workshops/sara_workshop.jpg';
+import vitalyWorkshop from '../images/workshops/vitaly_workshop.jpg';
 
 export default [
   { name: 'Atomic Design: Patterns and Principles', instructor: 'Brad Frost', image: atomicImage },
   { name: 'Roadmapping Your Product Roadmap', instructor: 'C Todd Lombardo', image: productImage },
+  { name: 'The Front-End Espresso Shot', instructor: 'Sara Soueidan', image: saraImage },
+  { name: 'Smart Responsive Design Patterns', instructor: 'Vitaly Friedman', image: vitalyWorkshop },
 ];
