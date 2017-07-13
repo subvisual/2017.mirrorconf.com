a35b9f219eb23d8f4141b85d07b7fdd0eb92b478
Miguel Palhas <mpalhas@gmail.com>
2017-07-13 19:23:51 +0100

    Adds mirror effect to venue text

--- Components/Venue.js
+++ Components/Venue.js
@@ -12,40 +12,43 @@ import DropcapText from '../Components/DropcapText';
 import SectionTitle from '../Components/SectionTitle';
 import ImageGradient from '../Components/ImageGradient';
 import SubSectionTitle from '../Components/SubSectionTitle';
+import Mirror from '../Components/Mirror';
 
 const Venue = () => (
   <div className="Venue">
-    <Section>
-      <div className="Venue-title">
-        <SectionTitle>The Venue</SectionTitle>
-      </div>
-      <div className="Grid Grid--1offset">
-        <div className="Grid-5column">
-          <div className="Venue-subtitle">
-            <SubSectionTitle>Theatro Circo, Braga, Portugal</SubSectionTitle>
-          </div>
-          <div className="Venue-description">
-            <DropcapText>
-              Mirror Conf will take place in Braga, a beautiful, friendly city
-              surrounded by stunning mountains and one of the oldest portuguese cities,
-              established in Roman times as Bracara Avgvsta.
-            </DropcapText>
-            <br />
-            <Text>
-              The venue for Mirror Conf will be Theatro Circo, a 20th-century,
-              Portuguese revivalist theatre and one of the most emblematic theatre in
-              Europe. It is located in one of the principal avenues in Braga, Avenida da
-              Liberdade.
-            </Text>
-            <Link href="www.google.pt" >Find your way here</Link>
+    <Mirror>
+      <Section>
+        <div className="Venue-title">
+          <SectionTitle>The Venue</SectionTitle>
+        </div>
+        <div className="Grid Grid--1offset">
+          <div className="Grid-5column">
+            <div className="Venue-subtitle">
+              <SubSectionTitle>Theatro Circo, Braga, Portugal</SubSectionTitle>
+            </div>
+            <div className="Venue-description">
+              <DropcapText>
+                Mirror Conf will take place in Braga, a beautiful, friendly city
+                surrounded by stunning mountains and one of the oldest portuguese cities,
+                established in Roman times as Bracara Avgvsta.
+              </DropcapText>
+              <br />
+              <Text>
+                The venue for Mirror Conf will be Theatro Circo, a 20th-century,
+                Portuguese revivalist theatre and one of the most emblematic theatre in
+                Europe. It is located in one of the principal avenues in Braga, Avenida da
+                Liberdade.
+              </Text>
+              <Link href="www.google.pt" >Find your way here</Link>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div className="Grid Grid--1offset">
-        <div className="Venue-image" style={ImageGradient(VenueImage)} />
-      </div>
-    </Section>
+        <div className="Grid Grid--1offset">
+          <div className="Venue-image" style={ImageGradient(VenueImage)} />
+        </div>
+      </Section>
+    </Mirror>
   </div>
 );
 
